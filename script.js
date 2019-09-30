6240b940c8ad77e97cc7321b59bee3fc78d4b1c8
Abdullah Omar <fishilike.fish@gmail.com>
2019-09-30 09:32:51 -0700

    task2

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 /* JavaScript code here */
+//Task1
 console.log("External JS")
+
+//Task 2
+
+function arraySum (arr){
+    let sum= 0
+    arr.forEach(element =>{
+        sum= sum + element
+
+    })
+    return (multiplier)=>{
+        return sum*multiplier
+    }
+}
+let sum = arraySum([1,2,3])
+console.log(sum)
